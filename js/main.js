4f61ac619f3f98b458d4a15ff3d1a24fb08cc6a6
Agne <agnepetrylaite@gmail.com>
2017-07-25 13:19:40 +0300

    initial

---
+++ js/main.js
@@ -0,0 +1,29 @@
+/**
+ * Created by agnė on 7/25/17.
+ */
+
+    var numbers = [
+    {type : 'number', value : 7, class : 'num-button seven'},
+    {type : 'number', value : 8, class : 'num-button eight'},
+    {type : 'number', value : 9, class : 'num-button nine'},
+
+    {type : 'number', value : 4, class : 'num-button four'},
+    {type : 'number', value : 5, class : 'num-button five'},
+    {type : 'number', value : 6, class : 'num-button six'},
+
+    {type : 'number', value : 1, class : 'num-button one'},
+    {type : 'number', value : 2, class : 'num-button two'},
+    {type : 'number', value : 3, class : 'num-button three'},
+
+    {type : 'number', value : 0, class : 'num-button zero'},
+    {type : 'number', value : '.' , class : 'num-button dot'},
+    {type : 'number', value : 'C', class : 'clear-button clear'},
+
+    {type : 'number', value : 'C', class : 'function-button add'},
+    {type : 'number', value : 'C', class : 'clear-button clear'},
+    {type : 'number', value : 'C', class : 'clear-button clear'},
+    {type : 'number', value : 'C', class : 'clear-button clear'},
+
+
+   
+];
