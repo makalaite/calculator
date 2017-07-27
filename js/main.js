93802439dd32052dea2d55b1b5c83055f858686a
Agne <agnepetrylaite@gmail.com>
2017-07-27 15:41:05 +0300

    function for disabling action buttons when started and action and funkc for enabling after

--- js/main.js
+++ js/main.js
@@ -6,31 +6,31 @@ let buttons = [
 
     {type: 'action', value: 'CE', cssClass: 'symbols'},
     {type: 'action', value: 'C', cssClass: 'symbols'},
-    {type: 'action', value: '+-', cssClass: 'symbols'},
-    {type: 'action', value: '%', cssClass: 'symbols'},
+    {type: 'action', value: '+-', cssClass: 'symbols disable'},
+    {type: 'action', value: '%', cssClass: 'symbols disable'},
 
 
     {type: 'number', value: 7, cssClass: 'numbers'},
     {type: 'number', value: 8, cssClass: 'numbers'},
     {type: 'number', value: 9, cssClass: 'numbers'},
-    {type: 'action', value: '/', cssClass: 'symbols'},
+    {type: 'action', value: '/', cssClass: 'symbols disable'},
 
 
     {type: 'number', value: 4, cssClass: 'numbers'},
     {type: 'number', value: 5, cssClass: 'numbers'},
     {type: 'number', value: 6, cssClass: 'numbers'},
-    {type: 'action', value: '*', cssClass: 'symbols'},
+    {type: 'action', value: '*', cssClass: 'symbols disable'},
 
     {type: 'number', value: 1, cssClass: 'numbers'},
     {type: 'number', value: 2, cssClass: 'numbers'},
     {type: 'number', value: 3, cssClass: 'numbers'},
-    {type: 'action', value: '-', cssClass: 'symbols'},
+    {type: 'action', value: '-', cssClass: 'symbols disable'},
 
 
     {type: 'number', value: '.', cssClass: 'symbols'},
     {type: 'number', value: 0, cssClass: 'numbers'},
-    {type: 'action', value: '+', cssClass: 'symbols'},
-    {type: 'action', value: '=', cssClass: 'symbols'}
+    {type: 'action', value: '+', cssClass: 'symbols disable'},
+    {type: 'action', value: '=', cssClass: 'symbols disable'}
 ];
 
 // $(document).ready(function () {
@@ -110,13 +110,73 @@ function handleClick(e) {
                     a += $b.val();      //paima a buvusia rieksme ir prideda nauja $b.val() salia!
                     $('input').val(a);
                 }
-
-
         }
+    } else {
+        if ($b.attr('type') === 'action') {
+
+            switch ($b.val()) {
+
+                case 'C':
+
+                    a = '0';
+                    $('input').val(a);
+                    $('.disable').attr('disabled', false);      //on renable action buttons
+                    break;
+
+                case 'CE':
+
+                    a = a.substring(0, a.length - 1);
+
+                    if (a.length < 1) {
+                        a = '0';
+                    }
+                    $('input').val(a);      //for deleting last string in value (wont work inside function)
+
+                    break;
+
+                case '+-':
+                    // console.log(a[0]);
+                    if (a[0] === '-') {
+
+                        a = a.substring(1, a.length);
+                        $('input').val(a);
 
 
+                    } else {
+                        if (a !== '0') {
+                            a = '-' + a;
+                            $('input').val(a);
+                        }
+
+                    }
+                    break;
+
+
+                case '+':
+                case '-':
+                case '*':
+                case '/':
+                case '%':
+
+                    action = $b.val();
+
+                    $('.disable').attr('disabled', true);
+                    a += ' ' + action + ' ';
+                    $('input').val(a);
+            }
+        }
     }
 }
+
+function updateNumber() {
+    if(!action) {
+        a += value;
+    } else {
+        b += value;
+    }
+}
+
+
 
 
 
