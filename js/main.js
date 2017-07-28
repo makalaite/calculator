5e42b9d5b565f92559d3b560664f83afec3624f1
Agne <agnepetrylaite@gmail.com>
2017-07-28 11:06:04 +0300

    code optimization with action cases

--- js/main.js
+++ js/main.js
@@ -50,6 +50,14 @@ let buttonField = $('#buttonField');
 
 $("<input disabled>").appendTo(buttonField);
 
+let ACTION_REPLACE = 'replaced',
+    ACTION_INCREASE = 'increased',
+    ACTION_REVERSE = 'reversed',
+    ACTION_CLEAR = 'cleared',
+    ACTION_DELETE_ONE = 'delete_one';
+
+
+
 
 for (let key in buttons) {
 
@@ -78,39 +86,8 @@ function handleClick(e) {
     let $b = $(e.currentTarget);                //like THIS
 
     if ($b.attr('type') === 'number') {
+        updateNumber(ACTION_INCREASE, $b.val());
 
-        switch ($b.val()) {
-
-            case '.':
-
-                if (a.indexOf('.') === -1) {        // -1=nera; naudojamas ant stringo ieskom simbolio ir grazina skaiciu(pozicija kur yra simbolis)
-
-                    a += $b.val();      //
-                    $('input').val(a);              //val function for putting number into input field
-                }
-                break;
-
-            case '0':
-
-                if (a.length === 1 && a === '0') {
-
-                } else {
-                    a += $b.val();
-                    $('input').val(a);
-                }
-                break;
-
-
-            default :
-
-                if (a.length === 1 && a === '0') {
-                    a = $b.val();          //perraso pries tai buvusia a reiksme. buna 0 ir deda nauja
-                    $('input').val(a);
-                } else {
-                    a += $b.val();      //paima a buvusia rieksme ir prideda nauja $b.val() salia!
-                    $('input').val(a);
-                }
-        }
     } else {
         if ($b.attr('type') === 'action') {
 
@@ -118,37 +95,41 @@ function handleClick(e) {
 
                 case 'C':
 
-                    a = '0';
-                    $('input').val(a);
-                    $('.disable').attr('disabled', false);      //on renable action buttons
+                    updateNumber(ACTION_CLEAR);
+                    // a = '0';
+                    // $('input').val(a);
+                    // $('.disable').attr('disabled', false);      //on renable action buttons
                     break;
 
                 case 'CE':
 
-                    a = a.substring(0, a.length - 1);
+                    updateNumber(ACTION_DELETE_ONE);
 
-                    if (a.length < 1) {
-                        a = '0';
-                    }
-                    $('input').val(a);      //for deleting last string in value (wont work inside function)
+                    // a = a.substring(0, a.length - 1);
+                    // if (a.length < 1) {
+                    //     a = '0';
+                    // }
+                    // $('input').val(a);      //for deleting last string in value (wont work inside function)
 
                     break;
 
                 case '+-':
-                    // console.log(a[0]);
-                    if (a[0] === '-') {
+                    updateNumber(ACTION_REVERSE);
 
-                        a = a.substring(1, a.length);
-                        $('input').val(a);
-
-
-                    } else {
-                        if (a !== '0') {
-                            a = '-' + a;
-                            $('input').val(a);
-                        }
-
-                    }
+                    // // console.log(a[0]);
+                    // if (a[0] === '-') {
+                    //
+                    //     // a = a.substring(1, a.length);
+                    //     // $('input').val(a);
+                    //
+                    //
+                    // } else {
+                    //     if (a !== '0') {
+                    //         // a = '-' + a;
+                    //         // $('input').val(a);
+                    //     }
+                    //
+                    // }
                     break;
 
 
@@ -161,19 +142,101 @@ function handleClick(e) {
                     action = $b.val();
 
                     $('.disable').attr('disabled', true);
-                    a += ' ' + action + ' ';
-                    $('input').val(a);
+                    updateNumber(ACTION_INCREASE, $b.val());
+                    // a += ' ' + action + ' ';
+                    // $('input').val(a);
             }
         }
     }
 }
 
-function updateNumber() {
-    if(!action) {
-        a += value;
+let numbers = ['0', ''];
+let actions = [];
+
+function updateNumber(action, value) {
+    // console.log(action, value);
+    /*if(!action) {
+            a += value;
     } else {
         b += value;
+    }*/
+    
+    
+
+    switch (action) {
+        case ACTION_DELETE_ONE:
+
+        break;
+
+
+        case ACTION_CLEAR:
+
+        break;
+
+
+        case ACTION_REVERSE:
+
+        break;
+
+
+        case ACTION_INCREASE:
+
+            let n = numbers[actions.length];        //istraukiamas skaicius is array tam kad nebesipliusuotu prie a kintamojo, po actiono naudojamas b kintamasis, po dar actiono c kintamasis..
+
+
+            switch (value) {
+
+                case '.':
+
+                    if (n.indexOf('.') === -1) {        // -1=nera; naudojamas ant stringo ieskom simbolio ir grazina skaiciu(pozicija kur yra simbolis)
+
+                        // a += $b.val();      //
+                        // $('input').val(a);              //val function for putting number into input field
+                    }
+                    break;
+
+                case '0':
+
+                    if (n.length === 1 && a === '0') {
+
+                    } else {
+                        updateNumber(ACTION_INCREASE, $b.val());
+                        // a += $b.val();
+                        // $('input').val(a);
+                    }
+                    break;
+
+
+                default :
+
+                    if (n.length === 1 && a === '0') {
+
+                        updateNumber(ACTION_REPLACE, $b.val());
+                        // a = $b.val();          //perraso pries tai buvusia a reiksme. buna 0 ir deda nauja
+                        // $('input').val(a);
+                    } else {
+
+                        updateNumber(ACTION_INCREASE, $b.val());
+                        // a += $b.val();      //paima a buvusia rieksme ir prideda nauja $b.val() salia!
+                        // $('input').val(a);
+                    }
+            }
+
+        numbers[actions.length] = n; //istrauktas sk idedamas atgal i array
+        break;
+
+
+        case ACTION_REPLACE:
+
+        break;
+
+        default: console.log('unknown action');
     }
+
+
+}
+function updateInput() {
+    
 }
 
 
